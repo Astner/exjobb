28fb3cd015282a23e0fce3811c0300267545da6e
Niklas Astner <niklasastner@astner.sics.se>
2016-09-08 15:38:20 +0200

    Restored single file version

--- pre-process_SPARK.py
+++ pre-process_SPARK.py
@@ -6,7 +6,8 @@ import os,time
 import shutil
 
 
-sc =SparkContext()
+sc = SparkContext()
+
 
 print("#####################################################")
 print("#####################################################")
@@ -16,25 +17,25 @@ print(sc.version)
 start_time = time.time()
 
 
-#devFile = 'yahoo/data/temp/testDataset_3_users_SPARK_VERSION.txt'
-#dev_10k = 'yahoo/data/temp/artistDataset_10k_users_SPARK_VERSION.txt'
-#dev_100k = 'yahoo/data/temp/artistDataset_100k_users_SPARK_VERSION.txt'
-#fullFile = 'yahoo/data/trainIdx1_SPARK_VERSION.txt'
+devFile = 'yahoo/data/temp/testDataset_3_users_SPARK_VERSION.txt'
+dev_10k = 'yahoo/data/temp/artistDataset_10k_users_SPARK_VERSION.txt'
+dev_100k = 'yahoo/data/temp/artistDataset_100k_users_SPARK_VERSION.txt'
+fullFile = 'yahoo/data/trainIdx1_SPARK_VERSION.txt'
 
 #TODO: add split versions and paths
 #ratingFile = sc.textFile(devFile)
 
 
 #inputPath = 'yahoo/data/temp'
-dev_1k = 'yahoo/data/temp/split_dataset_1k_users'
-dev_10k = 'yahoo/data/temp/split_dataset_10k_users'
-dev_100k = 'yahoo/data/temp/split_dataset_100k_users'
-dev_all = 'yahoo/data/temp/split_dataset_ALL_users'
+#dev_1k = 'yahoo/data/temp/split_dataset_1k_users'
+#dev_10k = 'yahoo/data/temp/split_dataset_10k_users'
+#dev_100k = 'yahoo/data/temp/split_dataset_100k_users'
+#dev_all = 'yahoo/data/temp/split_dataset_ALL_users'
 
 
-inputFolder = dev_100k
-outFolder = 'yahoo/data/ngram/split_dev_100k'
-
+#inputFolder = dev_100k
+inputFile = devFile
+outFolder = 'yahoo/data/ngram/dev'
 
 
 #outFile = 'yahoo/data/ngram/tempNRAM_3_users.txt'
@@ -85,82 +86,56 @@ print("#####################################################")
 if os.path.isdir(outFolder):
 	#os.rmdir(outFolder)
 	shutil.rmtree(outFolder)
-os.mkdir(outFolder) 
+#os.mkdir(outFolder) 
 
-logFile = outFolder + '/LOG_FILE' 
-log = open(logFile,'a')
+#logFile = outFolder + '/LOG_FILE' 
+#log = open(logFile,'a')
 
-i = 1
+#i = 1
 
-log.write('Initiating on file: ' + inputFolder +'\n')
+#log.write('Initiating on file: ' + inputFile +'\n')
 
 #import os
 #List all files in the folder & make sure they end in .txt
-for file in os.listdir(inputFolder):
-	if file.endswith(".txt"):
-		print(file)
+#for file in os.listdir(inputFolder):
+#	if file.endswith(".txt"):
+#		print(file)
 
-		ratingFile = sc.textFile(inputFolder +'/'+file)
+ratingFile = sc.textFile(inputFile)
 
-		userHistoryRDD = ratingFile.map(lambda line: splitAndRearange(line)).groupByKey()
+userHistoryRDD = ratingFile.map(lambda line: splitAndRearange(line)).groupByKey()
 
-		#print('\n\n\n\n User history created')
-		#print('\n\n\nCount on userHistory: %d' % (userHistoryRDD.count()))
-		#temp_time=time.time()-start_time
-		#print('\n\n\nuserHistory counted, time elapsed is: %d seconds == %d minutes' % (temp_time,temp_time/60))
-
-		ngramRDD = userHistoryRDD.flatMap(lambda line: userHistorytoNgram(line))
-
-		#print('\n\n\n\n Ngrams created')
-		#print('\n\n\nCount on ngrams: %d' % (ngramRDD.count()))
-		#temp_time=time.time()-start_time
-		#print('\n\n\nngrams counted, time elapsed is: %d seconds == %d minutes' % (temp_time,temp_time/60))
-
-
-		outputRDD = ngramRDD.reduceByKey(lambda a,b : a + b).map(lambda line: mapToOutputFormat(line))
-
-
-		outSubFolder = outFolder + '/split_nr_' + str(i)
-		i = i+1
-
-		outputRDD.saveAsTextFile(outSubFolder)
-		print('\n\n\nFile named: %s is done\n\n\n' %(file))
-		temp_time=time.time()-start_time
-		log.write('File named: %s is done, for i = %d, at time: %d minutes \n' %(file,i-1,temp_time/60))
-
-#print('\n\n\n\n Waiting to collect')
-#print('\n\n')
-#print(outputRDD.collect())
-#print('\n')
-#print('\n\n\nCount on output: %d' % (outputRDD.count()))
+#print('\n\n\n\n User history created')
+#print('\n\n\nCount on userHistory: %d' % (userHistoryRDD.count()))
 #temp_time=time.time()-start_time
-#print('\n\n\nOutput reduction completed, time elapsed is: %d seconds == %d minutes\n\n\n' % (temp_time,temp_time/60))
+#print('\n\n\nuserHistory counted, time elapsed is: %d seconds == %d minutes' % (temp_time,temp_time/60))
+
+ngramRDD = userHistoryRDD.flatMap(lambda line: userHistorytoNgram(line))
+
+#print('\n\n\n\n Ngrams created')
+#print('\n\n\nCount on ngrams: %d' % (ngramRDD.count()))
+#temp_time=time.time()-start_time
+#print('\n\n\nngrams counted, time elapsed is: %d seconds == %d minutes' % (temp_time,temp_time/60))
+
+
+outputRDD = ngramRDD.reduceByKey(lambda a,b : a + b).map(lambda line: mapToOutputFormat(line))
 
 
 
-#shutil.rmtree("dir-you-want-to-remove")
+
 #if os.path.isdir(outFolder):
 	#os.rmdir(outFolder)
 #	shutil.rmtree(outFolder)
 
-#Write to single .txt file to match CCM input
+#outputRDD.saveAsTextFile(outFolder)
 
-#outputList = outputRDD.collect()
-#print('\n\n\n Writing to file')
-
-#if os.path.isfile(outFile):
-#    os.remove(outFile)    
-#out = open(outFile,'a')
-
-#for line in outputList:
-#	out.write(line)
 
 
 end_time=time.time()-start_time
 print('All completed, end time is: %d seconds == %d minutes' %(end_time,end_time/60))
-log.write('All completed, end time is: %d seconds == %d minutes' %(end_time,end_time/60))
+#log.write('All completed, end time is: %d seconds == %d minutes' %(end_time,end_time/60))
 
-log.close()
+#log.close()
 print('\n\n\n\n')
 print("#####################################################")
 print("#####################################################")
