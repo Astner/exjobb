7956f8320ec50dac14778b5f55236c9339c40bb4
Niklas Astner <niklasastner@astner.sics.se>
2016-10-03 10:39:41 +0200

    Updated statisitcs on dataset distrubution

--- statistics_SPARK_data.py
+++ statistics_SPARK_data.py
@@ -24,9 +24,9 @@ fullFile = 'yahoo/data/trainIdx1_SPARK_VERSION.txt'
 
 
 
-inputFile = devFile
-outFolder = 'yahoo/data/statistics/dev'
-
+inputFile = fullFile
+#outFolder = 'yahoo/data/statistics/dev'
+outFolder = 'yahoo/data/statistics/full'
 
 print("#####################################################")
 
@@ -64,7 +64,7 @@ inputRDD = ratingFile.map(lambda line: extractTrackID(line))
 
 inputCount = inputRDD.count()
 temp_time=time.time()-start_time
-log.write('inputRatings counted, count is: %d \ntime elapsed is: %d seconds == %d minutes \n\n' % (inputCount,temp_time,temp_time/60))
+log.write('inputRating events counted, count is: %d \ntime elapsed is: %d seconds == %d minutes \n\n' % (inputCount,temp_time,temp_time/60))
 
 
 
@@ -74,32 +74,40 @@ reducedRDD = inputRDD.reduceByKey(lambda a,b : a + b)
 
 reducedCount = reducedRDD.count()
 temp_time=time.time()-start_time
-log.write('reduced form counted, count is: %d \ntime elapsed is: %d seconds == %d minutes \n\n' % (reducedCount,temp_time,temp_time/60))
+log.write('reduced to (item,count) counted, nr unique items is: %d \ntime elapsed is: %d seconds == %d minutes \n\n' % (reducedCount,temp_time,temp_time/60))
 
 
+#RUN Distribution counts
 
-
-filterRDD_1 = reducedRDD.filter(lambda v: v[1] >= 1 and v[1] < 5)
-#rdd.filter(lambda x: x % 2 == 0)
-
+filterRDD_1 = reducedRDD.filter(lambda v: v[1] >= 1 and v[1] < 20)
 filterCount = filterRDD_1.count()
 temp_time=time.time()-start_time
 log.write('filtered rdd nr 1 counted, count is: %d \ntime elapsed is: %d seconds == %d minutes \n\n' % (filterCount,temp_time,temp_time/60))
 
 
-filterRDD_2 = reducedRDD.filter(lambda v: v[1] > 5 and v[1] < 10)
-#rdd.filter(lambda x: x % 2 == 0)
-
+filterRDD_2 = reducedRDD.filter(lambda v: v[1] > 20 and v[1] < 50)
 filterCount_2 = filterRDD_2.count()
 temp_time=time.time()-start_time
 log.write('filtered rdd nr 2 counted, count is: %d \ntime elapsed is: %d seconds == %d minutes \n\n' % (filterCount_2,temp_time,temp_time/60))
 
 
+filterRDD_3 = reducedRDD.filter(lambda v: v[1] >= 50 and v[1] < 100)
+filterCount_3 = filterRDD_3.count()
+temp_time=time.time()-start_time
+log.write('filtered rdd nr 3 counted, count is: %d \ntime elapsed is: %d seconds == %d minutes \n\n' % (filterCount_3,temp_time,temp_time/60))
+
+
+filterRDD_4 = reducedRDD.filter(lambda v: v[1] >= 100)
+filterCount_4 = filterRDD_3.count()
+temp_time=time.time()-start_time
+log.write('filtered rdd nr 4 counted, count is: %d \ntime elapsed is: %d seconds == %d minutes \n\n' % (filterCount_4,temp_time,temp_time/60))
 
 
 
+#---------------------------------------------------
+#SAVE a filtered dataset
 rddFolder = outFolder +'/files'
-filterRDD_2.saveAsTextFile(rddFolder)
+#filterRDD_2.saveAsTextFile(rddFolder)
 
 
 end_time=time.time()-start_time
