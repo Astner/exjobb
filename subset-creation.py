5bcdff362b965795be65dd3b937e4fc639fdb99d
Niklas Astner <niklasastner@astner.sics.se>
2016-10-05 13:08:02 +0200

    Validation data check fixed

--- subset-creation.py
+++ subset-creation.py
@@ -19,13 +19,13 @@ fullValFile = 'yahoo/data/validationIdx1_SPARK_VERSION.txt'
 val_1k = 'yahoo/data/temp/dev_1k_users/validationIdx1_SPARK_VERSION.txt'
 
 ##############################################
-inputFile = devFile
-valFile = val_1k
+inputFile = fullFile
+valFile = fullValFile
 #outFolder = 'yahoo/data/ngram/dev'
 
 
-minItemOccurence = 2
-minUserHistory = 3
+minItemOccurence = 1200
+minUserHistory = 300
 
 #minItemOcc = sc.broadcast(minItemOccurence)
 #minUserHist = sc.broadcast(minUserHistory)
@@ -86,12 +86,12 @@ def splitAndRearange(line):
 
 
 def filterHistoryLength(userHistory,minCount):
-	#user = userHistory[0]
+	user = userHistory[0]
 	history = userHistory[1]
 
 	length = sum(1 for _ in history)
 	if length >= minCount:
-		return([(userHistory)])
+		return([(user,1)])
 	else:
 		return []
 
@@ -130,22 +130,51 @@ print('### Do validation data checks: ######################')
 validationRDD = sc.textFile(valFile)
 
 frequentValEventsRDD = validationRDD.map(lambda line: splitAndRearange(line))\
-									.filter(lambda line: line[1][0] in itemDict.value)
+									.filter(lambda line: line[1][0] in itemDict.value)\
+									.map(lambda line: (line[0],1))
+#Frequent users in form: (userID,1)
 
 
-for item in userLargeFrequentHistoryRDD.collect():
-	print(item)
+nrFreqValEvents = frequentValEventsRDD.count()
+print('\n\nNr frequent validation events: %d' % (nrFreqValEvents))
+
+#for item in userLargeFrequentHistoryRDD.collect():
+#	print(item)
+##print("#####################################################")
+#print("#####################################################")
+
+#for item in frequentValEventsRDD.collect():
+#	print(item)
+
+
+#usefulUserDataRDD = frequentValEventsRDD.groupWith(userLargeFrequentHistoryRDD)
+
+
+usefulUsersRDD = frequentValEventsRDD.union(userLargeFrequentHistoryRDD)\
+									 .reduceByKey(lambda a,b : a + b)\
+									 .filter(lambda line: line[1] >= 2)			
+
+
+#print(usefulUsersRDD.collect())
+
+#for item in usefulUserDataRDD.collect():
+#	user = item[0]
+#	a = item[1][0]
+#	b = item[1][1]
+#	print('\n %s' % (user))
+#	print('\n A = ')
+#	for obj in a:
+#		print(obj)
+#	print('\n B = ')
+##		print(obj)
+
+nrJoined = usefulUsersRDD.count()
+print('\n\nNr of useful users in joined RDD: %d' % (nrJoined))
+
+
+
 print("#####################################################")
 print("#####################################################")
-
-for item in frequentValEventsRDD.collect():
-	print(item)
-
-
-usefulUserDataRDD = frequentValEventsRDD.join(userLargeFrequentHistoryRDD)
-
-nrJoined = validationRDD.count()
-print('\n\nNr items in joined RDD: %d' % (nrJoined))
 
 
 
@@ -158,49 +187,18 @@ print('\n\nNr of users with small frequent histories: %d' % (nrSmallFreqHist))
 
 print('Nr of users with large frequent histories: %d' % (nrLargeHistories))
 
-print('\n\nNr items in joined RDD: %d' % (nrJoined))
+print('\n\nNr frequent validation events: %d' % (nrFreqValEvents))
 
 
-#oneHistory = userHistoryRDD.collect()[0][1] 
-#print('\n\n\n History length: %d' % (sum(1 for _ in oneHistory)))
-
-#for item in oneHistory:
-#	print(item)
+print('\n\nNr of useful users in union RDD: %d' % (nrJoined))
 
 
-#trainUserRDD = trainRDD.flatMap(lambda line: splitAndLabel(line,'history'))
-#valUserRDD = valRDD.flatMap(lambda line: splitAndLabel(line,'validation'))
-
-#print('\n\nNr of items in train: %d \n\n' % (trainUserRDD.count()))
-#print('\n\nNr of items in validation: %d \n\n' % (valUserRDD.count()))
-
-#usersRDD = trainUserRDD.union(valUserRDD).groupByKey()
-
-
-
-
+#print(usefulUsersRDD.map(lambda line: line[0]).collect())
 print("#####################################################")
+# Turn usefulUsers + frequent items into a new dataset to use
+# Save dataset to disk, should be able to put into pre-process_SPARK.py right away
 
 
 
 
-#inputCount = inputRDD.count()
-#temp_time=time.time()-start_time
-#log.write('inputRating events counted, count is: %d \ntime elapsed is: %d seconds == %d minutes \n\n' % (inputCount,temp_time,temp_time/60))
-
-
-
-
-#---------
-
-#Read dataset
-
-#Read file with list of frequent songs
-
-#Remove every item not in frequent itemss
-
-#Remove all users with no predict data or ti small history
-
-
-#Save dataset
 
